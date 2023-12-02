08f3ad041662c3dd9ec0acb3ac6c814fe411cd7a
Snehal Dutta <snehaldutta1230@gmail.com>
2023-12-03 00:39:34 +0100

    Plots updated

--- venv/plots.py
+++ venv/plots.py
@@ -5,13 +5,16 @@ df = pd.read_csv("data_2", delimiter=" ")
 df_new = df.set_index("size")
 
 df_new[["insertion","quicksort","heapsort","radixsort"]].plot(linewidth=4)
+plt.savefig("plt1.png")
 df_new[["insertion_sorted","quicksort_sorted","heapsort_sorted","radixsort_sorted"]].plot(linewidth=4)
-plt.show()
+plt.savefig("plt2.png")
+
 
 small = df_new[18:]
 
 small[["quicksort","heapsort","radixsort"]].plot(linewidth=4)
+plt.savefig("plt3.png")
 small[["quicksort_sorted","heapsort_sorted","radixsort_sorted"]].plot(linewidth=4)
-plt.show()
+plt.savefig("plt4.png")
 
 
